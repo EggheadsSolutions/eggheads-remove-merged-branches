08615346703bfd58a377a9d6b20085bd42b1b13a
Igor Smirnov <idecme92@yandex.ru>
2022-11-30 11:45:27 +1000

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+HEAD_BRANCH=${INPUT_HEAD_BRANCH##*/}
+branches=`git branch -r --merged $HEAD_BRANCH | grep -v "main$" | grep -v "master$" | grep -v "production$" | grep -v "staging$" | grep -v -i "$INPUT_PROTECTED_BRANCH"`
+for branch in $branches; do
+    branch=`echo $branch | sed 's/origin\///g'`
+    if [[ $branch != $HEAD_BRANCH && $branch != $INPUT_PROTECTED_BRANCH ]]; then
+        git push origin --delete $branch
+        echo "$branch removed."
+    else
+        echo "Cannot remove $branch."
+    fi
+done
